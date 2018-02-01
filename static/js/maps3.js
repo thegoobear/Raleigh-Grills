48fcdc251f0432a7478b0acf4e6c2848fadd0624
Tripp Green <vertusmedia@gmail.com>
2018-01-31 21:35:33 -0500

    fix semicolon in maps3

--- static/js/maps3.js
+++ static/js/maps3.js
@@ -130,11 +130,6 @@ else {
 function initMap() {
   var map = new google.maps.Map(document.getElementById('map'));
 
-  map.onerror = function (){
-    console.log('poo');
-    alert('Error: ' + status);
-  }
-
   var addresslist = [];
   var counter = 0;
   //Geocoder for getting lat and longitude from grill name/location
